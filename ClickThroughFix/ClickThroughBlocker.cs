4716129810a40b2426e3f7870b787b149c5f7ff6
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-02-10 17:14:32 -0500

    Initial full release Updated docs Added public function MouseIsOverWindow

---
+++ ClickThroughFix/ClickThroughBlocker.cs
@@ -0,0 +1,235 @@
+﻿using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ClickThroughFix
+{
+
+    public static class ClickThruBlocker
+    {
+        // Most of this is from JanitorsCloset, ImportExportSelect.cs
+
+        public class CTBWin
+        {
+            //public Rect rect;
+            int id;
+
+            internal bool weLockedEditorInputs = false;
+            internal bool weLockedFlightInputs = false;
+            internal string windowName;
+            internal string lockName;
+            internal long lastLockCycle;
+
+
+            public CTBWin(int id, Rect screenRect, string winName, string lockName)
+            {
+                this.id = id;
+                this.windowName = winName;
+                this.lockName = lockName;
+
+            }
+
+            public void SetLockString(string s)
+            {
+                this.lockName = s;
+            }
+
+            public static bool MouseIsOverWindow(Rect rect)
+            {
+                return   rect.Contains(new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y));
+            }
+
+            //Lifted this more or less directly from the Kerbal Engineer source. Thanks cybutek!
+            internal void PreventEditorClickthrough(Rect r)
+            {
+                Log.Info("ClickThruBlocker: PreventEditorClickthrough");
+                bool mouseOverWindow = MouseIsOverWindow(r);
+                if (mouseOverWindow)
+                {
+                    if (!weLockedEditorInputs)
+                    {
+                        Log.Info("ClickThruBlocker: PreventEditorClickthrough, locking on window: " + windowName);
+                        EditorLogic.fetch.Lock(true, true, true, lockName);
+                        weLockedEditorInputs = true;
+                    }
+                    lastLockCycle = OnGUILoopCount.GetOnGUICnt(); 
+                   
+                }
+                if (!weLockedEditorInputs || mouseOverWindow) return;
+                Log.Info("ClickThruBlocker: PreventEditorClickthrough, unlocking on window: " + windowName);
+                EditorLogic.fetch.Unlock(lockName);
+                weLockedEditorInputs = false;
+            }
+
+            // Following lifted from MechJeb
+            internal void PreventInFlightClickthrough(Rect r)
+            {
+                //Log.Info("ClickThruBlocker: PreventInFlightClickthrough");
+                bool mouseOverWindow = MouseIsOverWindow(r);
+                if (mouseOverWindow)
+                {
+                    if (!weLockedFlightInputs && !Input.GetMouseButton(1))
+                    {
+                        Log.Info("ClickThruBlocker: PreventInFlightClickthrough, locking on window: " + windowName); ;
+
+                        InputLockManager.SetControlLock(ControlTypes.ALLBUTCAMERAS, lockName);
+                        weLockedFlightInputs = true;
+                       
+                    }
+                    if (weLockedFlightInputs)
+                        lastLockCycle = OnGUILoopCount.GetOnGUICnt();
+                }
+                if (weLockedFlightInputs && !mouseOverWindow)
+                {
+                    Log.Info("ClickThruBlocker: PreventInFlightClickthrough, unlocking on window: " + windowName);
+                    InputLockManager.RemoveControlLock(lockName);
+                    weLockedFlightInputs = false;
+                }
+            }
+
+            private void OnDestroy()
+            {
+                Log.Info("ClickThruBlocker: OnDestroy");
+                winList.Remove(id);
+                if (weLockedEditorInputs)
+                {
+                    EditorLogic.fetch.Unlock(lockName);
+                    weLockedEditorInputs = false;
+                }
+                if (weLockedFlightInputs)
+                {
+                    InputLockManager.RemoveControlLock(lockName);
+                    weLockedFlightInputs = false;
+                }
+            }
+        }
+
+        internal static Dictionary<int, CTBWin> winList = new Dictionary<int, CTBWin>();
+
+        private static Rect UpdateList(int id, Rect rect, string text)
+        {
+            CTBWin win = null;
+            if (!winList.TryGetValue(id, out win))
+            {
+                win = new CTBWin(id, rect, text, text);
+                winList.Add(id, win);
+            }
+
+            if (HighLogic.LoadedSceneIsEditor)
+                win.PreventEditorClickthrough(rect);
+            if (HighLogic.LoadedSceneIsFlight || HighLogic.LoadedSceneHasPlanetarium)
+                win.PreventInFlightClickthrough(rect);
+
+            return rect;
+        }     
+    
+        public static Rect GUILayoutWindow(int id, Rect screenRect, GUI.WindowFunction func, string text, GUIStyle style, params GUILayoutOption[] options)
+        {
+            Rect r = GUILayout.Window(id, screenRect, func, text, style, options);
+
+            return UpdateList(id, r, text); 
+        }
+
+        public static Rect GUILayoutWindow(int id, Rect screenRect, GUI.WindowFunction func, string text, params GUILayoutOption[] options)
+        {
+            Rect r = GUILayout.Window(id, screenRect, func, text, options);
+
+            return UpdateList(id, r, text);
+        }
+
+        public static Rect GUILayoutWindow(int id, Rect screenRect, GUI.WindowFunction func, GUIContent content, params GUILayoutOption[] options)
+        {
+            Rect r = GUILayout.Window(id, screenRect, func, content, options);
+
+            return UpdateList(id, r, id.ToString());
+        }
+
+        public static Rect GUILayoutWindow(int id, Rect screenRect, GUI.WindowFunction func, Texture image, params GUILayoutOption[] options)
+        {
+            Rect r = GUILayout.Window(id, screenRect, func, image, options);
+
+            return UpdateList(id, r, id.ToString());
+        }
+
+        public static Rect GUIWindow(int id, Rect clientRect, GUI.WindowFunction func, Texture image, GUIStyle style)
+        {
+            Rect r = GUI.Window(id, clientRect, func, image, style);
+            return UpdateList(id, r, id.ToString());
+
+        }
+        public static Rect GUIWindow(int id, Rect clientRect, GUI.WindowFunction func, string text, GUIStyle style)
+        {
+            Rect r = GUI.Window(id, clientRect, func, text, style);
+            return UpdateList(id, r, text);
+        }
+        public static Rect GUIWindow(int id, Rect clientRect, GUI.WindowFunction func, GUIContent content)
+        {
+            Rect r = GUI.Window(id, clientRect, func, content);
+            return UpdateList(id, r, id.ToString());
+        }
+        public static Rect GUIWindow(int id, Rect clientRect, GUI.WindowFunction func, Texture image)
+        {
+            Rect r = GUI.Window(id, clientRect, func, image);
+            return UpdateList(id, r, id.ToString());
+        }
+        public static Rect GUIWindow(int id, Rect clientRect, GUI.WindowFunction func, string text)
+        {
+            Rect r = GUI.Window(id, clientRect, func, text);
+            return UpdateList(id, r, text);
+        }
+        public static Rect GUIWindow(int id, Rect clientRect, GUI.WindowFunction func, GUIContent title, GUIStyle style)
+        {
+            Rect r = GUI.Window(id, clientRect, func, title, style);
+            return UpdateList(id, r, title.ToString());
+        }
+    }
+
+    [KSPAddon(KSPAddon.Startup.Flight, true)]
+    internal class OnGUILoopCount : MonoBehaviour
+    {
+        static long onguiCnt = 0;
+
+
+        internal static long GetOnGUICnt()
+        {
+            return onguiCnt;
+        }
+        private void Start()
+        {
+            DontDestroyOnLoad(this);
+            InvokeRepeating("DoGuiCounter",5.0f, 0.25f);
+        }
+        private void OnDestroy()
+        {
+
+        }
+        private void DoGuiCounter()
+        {
+            long lastonGuiCnt = (onguiCnt++) - 1;
+
+            //for (int i = 0; i < ClickThruBlocker.winList.Count; i++)
+            foreach (var win in ClickThruBlocker.winList.Values)
+            {
+                if (win.lastLockCycle < lastonGuiCnt)
+                {
+                    Log.Info("lastonGuiCnt: " + lastonGuiCnt + "lastLockCycle: " + win.lastLockCycle);
+                    {
+                        if (win.weLockedEditorInputs)
+                        {
+                            EditorLogic.fetch.Unlock(win.lockName);
+                            win.weLockedEditorInputs = false;
+                        }
+                        if (win.weLockedFlightInputs)
+                        {
+                            InputLockManager.RemoveControlLock(win.lockName);
+                            win.weLockedFlightInputs = false;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+}
---
+++ ClickThroughFix/Log.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ClickThroughFix
+{
+    public static class Log
+    {
+        public enum LEVEL
+        {
+            OFF = 0,
+            ERROR = 1,
+            WARNING = 2,
+            INFO = 3,
+            DETAIL = 4,
+            TRACE = 5
+        };
+        static string PREFIX = "ClickThroughBlocker";
+
+        public static void setTitle(string t)
+        {
+            PREFIX = t + ": ";
+        }
+
+        public static LEVEL level = LEVEL.INFO;
+
+
+
+        public static LEVEL GetLevel()
+        {
+            return level;
+        }
+
+        public static void SetLevel(LEVEL level)
+        {
+            UnityEngine.Debug.Log("log level " + level);
+            Log.level = level;
+        }
+
+        public static LEVEL GetLogLevel()
+        {
+            return level;
+        }
+
+        private static bool IsLevel(LEVEL level)
+        {
+            return level == Log.level;
+        }
+
+        public static bool IsLogable(LEVEL level)
+        {
+            return level <= Log.level;
+        }
+
+        public static void Trace(String msg)
+        {
+            if (IsLogable(LEVEL.TRACE))
+            {
+                UnityEngine.Debug.Log(PREFIX + msg);
+            }
+        }
+
+        public static void Detail(String msg)
+        {
+            if (IsLogable(LEVEL.DETAIL))
+            {
+                UnityEngine.Debug.Log(PREFIX + msg);
+            }
+        }
+
+        [ConditionalAttribute("DEBUG")]
+        public static void Info(String msg)
+        {
+
+            if (IsLogable(LEVEL.INFO))
+            {
+                UnityEngine.Debug.Log(PREFIX + msg);
+            }
+        }
+
+        //        [ConditionalAttribute("DEBUG")]
+        public static void Test(String msg)
+        {
+            //if (IsLogable(LEVEL.INFO))
+            {
+                UnityEngine.Debug.LogWarning(PREFIX + "TEST:" + msg);
+            }
+        }
+
+
+        public static void Warning(String msg)
+        {
+            if (IsLogable(LEVEL.WARNING))
+            {
+                UnityEngine.Debug.LogWarning(PREFIX + msg);
+            }
+        }
+
+        public static void Error(String msg)
+        {
+            if (IsLogable(LEVEL.ERROR))
+            {
+                UnityEngine.Debug.LogError(PREFIX + msg);
+            }
+        }
+
+        public static void Exception(Exception e)
+        {
+            Log.Error("exception caught: " + e.GetType() + ": " + e.Message);
+        }
+
+    }
+}
