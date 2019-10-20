03c30bf287c56009ee99a45327a8f8fc80d0f10e
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-10-20 00:07:07 -0400

    Updated deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -7,7 +7,7 @@ rem GAMEDATA is the name of the local GameData
 rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
 rem    but not always
 
-set H=$(KSPDIR)
+set H=%KSPDIR%
 set GAMEDIR=000_ClickThroughBlocker
 set GAMEDATA="GameData\"
 set VERSIONFILE=%GAMEDIR%.version
